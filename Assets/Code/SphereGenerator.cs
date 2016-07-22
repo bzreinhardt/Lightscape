1771cd3cbfef70af92206cbaae55e024680775a6
bzreinhardt <bzreinhardt@gmail.com>
2016-07-22 02:38:22 -0700

    working everything!

--- Assets/Code/SphereGenerator.cs
+++ Assets/Code/SphereGenerator.cs
@@ -8,58 +8,58 @@ public class SphereGenerator : MonoBehaviour {
 	public GameObject target;
 	public static GameObject sphereTarget;
 	public GameObject gravtiyCenter;
-
+    public GameObject head;
+    public int numSpheres = 200;
+    bool initialized;
 
 	// Use this for initialization
 	void Start () {
-		sphereTarget = target;
-		//random torus
-		GameObject torus = new GameObject();
-		if (randomize) {
-			for (int i = 0; i < 200; i++) {
-				float theta = Random.Range (0, 2 * Mathf.PI);
-				float phi = Random.Range (0, 2 * Mathf.PI);
-                Vector3 torusCenter = gameObject.transform.position;
-				GameObject star = Instantiate (target);
-				star.gameObject.transform.SetParent (torus.gameObject.transform);
-				float x = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Cos(phi) + torusCenter.x;
-				float z = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Sin(phi) + torusCenter.y;
-				float y = tubeRadius * Mathf.Sin (theta) + torusCenter.z;
-				Vector3 r = new Vector3 (x, y, z);
-				star.gameObject.transform.position = r;
-				star.GetComponent<GravityObject> ().target = gravtiyCenter.GetComponent<GravityObject>();
-				float speed = Mathf.Sqrt (GravityObject.G*gravtiyCenter.GetComponent<Rigidbody>().mass/
-					r.magnitude);
-				Vector3 direction = Vector3.Cross (new Vector3 (0, 1, 0), r);
-				direction.Normalize();
-				//star.GetComponent<Rigidbody> ().velocity = speed * direction;
-			}
-		} else {
-		//non random torus
-			for (int i = 0; i < 10; i++) {
-				float theta = i * 2 * Mathf.PI/10;
-				for (int j = 0; j < 10; j++) {
-					float phi = j * 2 * Mathf.PI / 10;
-					GameObject star = Instantiate (target);
-					star.gameObject.transform.SetParent (torus.gameObject.transform);
-					float x = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Cos(phi);
-					float z = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Sin(phi);
-					float y = tubeRadius * Mathf.Sin (theta);
-					star.gameObject.transform.position = new Vector3 (x, y, z);
-				}
-			} 
-		}
-
-
-	
+        initialized = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (!initialized && head.GetComponent<SteamVR_TrackedObject>().isValid)
+        {
+            Initialize();
+        }
 	}
 
 	public static GameObject Generate () {
 		return Instantiate (sphereTarget);
 	}
+
+    void Initialize()
+    {
+        sphereTarget = target;
+        //random torus
+        GameObject torus = new GameObject();
+        torus.transform.position = head.transform.position + head.transform.forward*0.7f + head.transform.up * -0.4f;
+        torus.name = "torus";
+        gravtiyCenter.transform.position = torus.transform.position;
+        if (randomize)
+        {
+            for (int i = 0; i < numSpheres; i++)
+            {
+                float theta = Random.Range(0, 2 * Mathf.PI);
+                float phi = Random.Range(0, 2 * Mathf.PI);
+                // Start 0.5 meters in front of the headest
+                GameObject star = Instantiate(target);
+                star.gameObject.transform.SetParent(torus.gameObject.transform);
+                float x = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Cos(phi);
+                float z = (bigRadius + tubeRadius * Mathf.Cos(theta)) * Mathf.Sin(phi);
+                float y = tubeRadius * Mathf.Sin(theta);
+                Vector3 r = new Vector3(x, y, z);
+                star.gameObject.transform.localPosition = r;
+
+                star.GetComponent<GravityObject>().target = gravtiyCenter.GetComponent<GravityObject>();
+                float speed = Mathf.Sqrt(GravityObject.G * gravtiyCenter.GetComponent<Rigidbody>().mass /
+                    r.magnitude);
+                Vector3 direction = Vector3.Cross(new Vector3(0, 1, 0), r);
+                direction.Normalize();
+                //star.GetComponent<Rigidbody> ().velocity = speed * direction;
+            }
+        }
+        initialized = true;
+    }
 }
--- Assets/PinchZoomer.cs
+++ Assets/PinchZoomer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class PinchZoomer : MonoBehaviour {
@@ -11,15 +12,29 @@ public class PinchZoomer : MonoBehaviour {
     Vector3 rightAnchor;
     Vector3 leftAnchor;
     float prevDiameter;
-    float epsilon = 0.01f;
-    public bool debug = false;
-    void Awake()
+    public float epsilon = 0.0001f;
+    public int zoomLevel;
+    public GameObject zoomSphere;
+    public float zoomRadius;
+    public int minZoomSpheres = 0;
+    public int maxZoomSpheres = 4;
+    Dictionary<int, List<GameObject>> newSpheres;
+    bool continuousPress;
+    float zoomScale;
+    Vector3 center;
+
+    void Start()
     {
-        
+        zoomLevel = 0;
+        zoomRadius = 0.7f;
+        newSpheres = new Dictionary<int, List<GameObject>>();
+        continuousPress = false;
+        center = new Vector3();
     }
 
     void FixedUpdate()
     {
+        // Set up Controller inputs.
         if (rightTrackedObject == null)
         {
             if (rightController.GetComponent<SteamVR_TrackedObject>().isValid)
@@ -41,34 +56,129 @@ public class PinchZoomer : MonoBehaviour {
         }
         var leftInput = SteamVR_Controller.Input((int)leftTrackedObject.index);
         var rightInput = SteamVR_Controller.Input((int)rightTrackedObject.index);
+        // reset everything
+        if (rightInput.GetPress(Valve.VR.EVRButtonId.k_EButton_Grip) || leftInput.GetPress(Valve.VR.EVRButtonId.k_EButton_Grip))
+        {
+            var objects = GameObject.FindGameObjectsWithTag("zoomable");
+            foreach (var obj in objects)
+            {
+                obj.transform.position = obj.GetComponent<SphereMemory>().originalPosition;
+            }
+            foreach (int key in newSpheres.Keys)
+            {
+                DestroySpheres(key);
+            }
+        }
 
-        // Set an anchor when one trigger is down
+        // Zoom when both triggers are pulled
         if (rightInput.GetHairTrigger() && leftInput.GetHairTrigger())
         {
-            Debug.Log("both triggers pressed");
             rightAnchor = rightController.transform.position;
             leftAnchor = leftController.transform.position;
             
             float d = (rightAnchor - leftAnchor).magnitude;
-            bool expanding = false;
-            bool contracting = false;
-            if (d > prevDiameter + epsilon)
+            int change = 0;
+            if (!continuousPress)
             {
-                expanding = true;
-                Debug.Log("expanding");
-            } else if (d < prevDiameter - epsilon)
-            {
-                contracting = true;
-                Debug.Log("contracting");
+                prevDiameter = d;
+                center = (rightAnchor - leftAnchor) / 2.0f + leftAnchor;
             }
-            prevDiameter = d;
-            if (debug)
+            float delta = d - prevDiameter;
+            if (delta > epsilon)
             {
-                testSphere.transform.localScale = new Vector3(d, d, d);
-                testSphere.transform.position = (rightAnchor - leftAnchor) / 2.0f + leftAnchor;
+                change = 1;
+            } else if (delta < -epsilon)
+            {
+                change = -1;
             }
+            // reset the zoom level if you had let go of the controllers
             
+            zoomLevel = zoomLevel + change;
+            prevDiameter = d;
+            // Generate some spheres near the hit point
+            if (change > 0)
+            {
+                GenerateSpheres(center);
+            } else
+            {
+                DestroySpheres(zoomLevel);
+            }
+
+            var objects = GameObject.FindGameObjectsWithTag("zoomable");
+            foreach (var obj in objects)
+            {
+                Vector3 rtp = EuclideanToSpherical(obj.GetComponent<SphereMemory>().oldPosition - center);
+                // move radially towards or away from the gesture center
+                rtp.x = ZoomFunction(change, rtp.x);
+                obj.transform.position = SphericalToEuclidean(rtp) + center;
+                obj.GetComponent<SphereMemory>().oldPosition = obj.transform.position;
+            }
+            continuousPress = true;
+        } else
+        {
+            var objects = GameObject.FindGameObjectsWithTag("zoomable");
+            foreach (var obj in objects)
+            {
+                obj.GetComponent<SphereMemory>().oldPosition = obj.transform.position;
+            }
+            // record whether you've been continuously dragging the controllers
+            continuousPress = false;
         }
      
+    }
+
+    float ZoomFunction(int change, float r)
+    {
+        // adjust how much faster small distances expand than large ones
+        float smallExplosionAdjust = 0.2f;
+        // scale the rate of expansion
+        float scale = 0.01f;
+        return r * Mathf.Exp(change * scale / (smallExplosionAdjust + r));
+    }
+
+    Vector3 EuclideanToSpherical(Vector3 xyz)
+    {
+        float r = xyz.magnitude;
+        float theta = Mathf.Acos(xyz.y / r);
+
+        float phi = Mathf.Atan2(xyz.z, xyz.x);
+        return new Vector3(r, theta, phi);
+    }
+
+    Vector3 SphericalToEuclidean(Vector3 rtp)
+    {
+        float x = rtp.x * Mathf.Sin(rtp.y) * Mathf.Cos(rtp.z);
+        float y = rtp.x * Mathf.Cos(rtp.y);
+        float z = rtp.x * Mathf.Sin(rtp.y) * Mathf.Sin(rtp.z);
+        return new Vector3(x, y, z);
+    }
+
+    void GenerateSpheres(Vector3 center)
+    {
+        for (int i = 0; i < Random.Range(minZoomSpheres, maxZoomSpheres); i++)
+        {
+            GameObject newSphere = SphereGenerator.Generate();
+            Vector3 delta = new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f);
+            delta = 0.01f * delta;
+            newSphere.GetComponent<SphereMemory>().SetOriginalPosition(center + delta);
+
+            if (!newSpheres.ContainsKey(zoomLevel))
+            {
+                newSpheres.Add(zoomLevel, new List<GameObject>());
+            }
+            newSpheres[zoomLevel].Add(newSphere);
+        }
+    }
+
+    void DestroySpheres(int level)
+    {
+        // turn off spheres in the order they were created as you zoom back out
+        if (newSpheres.ContainsKey(level))
+        {
+            foreach (GameObject sphere in newSpheres[level])
+            {
+                sphere.SetActive(false);
+            }
+        }
     }
 }
